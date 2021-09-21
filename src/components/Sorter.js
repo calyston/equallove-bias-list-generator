a722b76cbbc2f91a7e2e46adb96bee2bdd6ebf43
Christine <calyston.14@gmail.com>
2021-09-21 22:10:58 +0100

    feat: sorter function added

---
+++ src/components/Sorter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Sorter = () => {
+  return (
+    <main>
+      <section id="question-container">
+        <figure id="optionA">
+        </figure>
+        <span>or</span>
+        <figure id="optionB">
+        </figure>
+      </section>
+      <section id="results-container">
+        <h2>Result:</h2>
+        <ol id="results-list"></ol>
+        <h2 id="retry">Retry?</h2>
+      </section>
+    </main>
+  )
+};
