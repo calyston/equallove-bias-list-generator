ba818ce785ab65c26c3f3b58aa7b2e7cd9b7803a
Christine <calyston.14@gmail.com>
2021-09-21 22:55:14 +0100

    feat: group member array added to sorter

--- src/components/Sorter.js
+++ src/components/Sorter.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Sorter = () => {
+
+  const [groupMembers, setGroupMembers] = useState(
+    [
+      { name: 'Yamamoto Anna', photo: '' },
+      { name: 'Morohashi Sana', photo: '' },
+      { name: 'Otani Emiri', photo: '' },
+      { name: 'Otoshima Risa', photo: '' },
+      { name: 'Sasaki Maika', photo: '' },
+      { name: 'Oba Hana', photo: '' },
+      { name: 'Noguchi Iori', photo: '' },
+      { name: 'Takamatsu Hitomi', photo: '' },
+      { name: 'Takiwaki Shoko', photo: '' },
+      { name: 'Saito Nagisa', photo: '' },
+      { name: 'Saito Kiara', photo: '' }
+    ]
+  );
+
+
   return (
     <main>
       <section id="question-container">
