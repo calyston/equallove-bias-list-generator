7f566612a607bf111066709686440cdfff5d7430
Christine <calyston.14@gmail.com>
2021-09-23 16:04:06 +0100

    feat: memmber photos added to array

--- src/components/Sorter.js
+++ src/components/Sorter.js
@@ -1,20 +1,33 @@
 import React, { useState, useEffect } from 'react';
 
+//Import Images
+import wcAnna from '../assets/images/weekend-citron/Yamamoto_anna_citron.webp';
+import wcSana from '../assets/images/weekend-citron/Morohashi_sana_citron.webp';
+import wcEmiri from '../assets/images/weekend-citron/Otani_emiri_citron.webp';
+import wcRisa from '../assets/images/weekend-citron/Otoshima_risa_citron.webp';
+import wcMaika from '../assets/images/weekend-citron/Sasaki_maika_citron.webp';
+import wcHana from '../assets/images/weekend-citron/Oba_hana_citron.webp';
+import wcIori from '../assets/images/weekend-citron/Noguchi_iori_citron.webp';
+import wcHitomi from '../assets/images/weekend-citron/Takamatsu_hitomi_citron.webp';
+import wcShoko from '../assets/images/weekend-citron/Takiwaki_shoko_citron.webp';
+import wcNagisa from '../assets/images/weekend-citron/Saito_nagisa_citron.webp';
+import wcKiara from '../assets/images/weekend-citron/Saito_kiara_citron.webp';
+
 const Sorter = () => {
 
   const [groupMembers, setGroupMembers] = useState(
     [
-      { name: 'Yamamoto Anna', photo: '' },
-      { name: 'Morohashi Sana', photo: '' },
-      { name: 'Otani Emiri', photo: '' },
-      { name: 'Otoshima Risa', photo: '' },
-      { name: 'Sasaki Maika', photo: '' },
-      { name: 'Oba Hana', photo: '' },
-      { name: 'Noguchi Iori', photo: '' },
-      { name: 'Takamatsu Hitomi', photo: '' },
-      { name: 'Takiwaki Shoko', photo: '' },
-      { name: 'Saito Nagisa', photo: '' },
-      { name: 'Saito Kiara', photo: '' }
+      { name: 'Yamamoto Anna', photo: wcAnna },
+      { name: 'Morohashi Sana', photo: wcSana },
+      { name: 'Otani Emiri', photo: wcEmiri },
+      { name: 'Otoshima Risa', photo: wcRisa },
+      { name: 'Sasaki Maika', photo: wcMaika },
+      { name: 'Oba Hana', photo: wcHana },
+      { name: 'Noguchi Iori', photo: wcIori },
+      { name: 'Takamatsu Hitomi', photo: wcHitomi },
+      { name: 'Takiwaki Shoko', photo: wcShoko },
+      { name: 'Saito Nagisa', photo: wcNagisa },
+      { name: 'Saito Kiara', photo: wcKiara }
     ]
   );
 
