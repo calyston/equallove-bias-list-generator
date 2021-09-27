16b7f26e06134a419d0f5da1c44574ab453f5960
Christine <calyston.14@gmail.com>
2021-09-27 18:09:21 +0100

    style: logo added and font size changed to rem

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+//Import Logo
+import logo from '../assets/images/logo_heart.png';
+
 const Header = () => {
   return (
     <header>
-      {/* <img id="logo" alt="" /> */}
+      <img id="logo" src={logo} alt="#" />
       <h2>Bias List Generator</h2>
       <p id="how-to">How to play: Each member of =LOVE will go head-to-head; select your favourite member each time to
         determine your
